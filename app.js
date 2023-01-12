0f73292658d04f59112c27ec63cf3013215ba3f0
jihwanAn <an0572@naver.com>
2023-01-12 21:43:08 +0900

    배열

---
+++ app.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3];
+console.log(numbers);
+
+const moreNumbers = Array(5, 2);
+console.log(moreNumbers);
+
+const yetMoreNumbers = Array.of(1, 2);
+console.log(yetMoreNumbers);
