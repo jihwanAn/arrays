d51f6c7b400c9d8cab6c1020505ea97db73f28ad
jihwanAn <an0572@naver.com>
2023-01-13 20:33:06 +0900

    Maps 와 객체

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // const numbers = [1, 2, 3];
 // console.log(numbers);
 
-// // const moreNumbers = Array(5, 2);
-// // console.log(moreNumbers);
+// const moreNumbers = Array(5, 2);
+// console.log(moreNumbers);
 
-// // const yetMoreNumbers = Array.of(1, 2);
-// // console.log(yetMoreNumbers);
+// const yetMoreNumbers = Array.of(1, 2);
+// console.log(yetMoreNumbers);
 
 // const listItems = document.querySelectorAll("li");
 // console.log(listItems);
@@ -32,23 +32,23 @@
 // const hobbies = ["Sports", "Cooking"];
 // hobbies.push("Reading"); // 새 요소 추가
 // hobbies.unshift("Coding"); // 첫 부분에 추가
-// const poppedValue = hobbies.pop(); //마지막 요소 삭제
+// const poppedValue = hobbies.pop(); // 마지막 요소 삭제
 // console.log(poppedValue); // 위 삭제 요소 반환
 // hobbies.shift(); // 첫 요소 삭제
 // console.log(hobbies);
 
 // hobbies[1] = "Coding";
-// // hobbies[5] = "Reading";
+// hobbies[5] = "Reading";
 // console.log(hobbies, hobbies[5]);
 
-// hobbies.splice(0, 0, "Good Food"); //실제 배열에서만 사용 (이터러블, 배열 같은 객체 안됨)
+// hobbies.splice(0, 0, "Good Food"); // * 실제 배열에서만 사용 (이터러블, 배열 같은 객체 안됨)
 // console.log(hobbies);
 // const removedElement = hobbies.splice(0, 1);
 // console.log(removedElement);
 
 // const testResults = [1, 2.2, 3.3, 4.4, -5, 10];
-// // const storedResults = testResults.slice(0, 4); // 배열을 복사, 새 주소를 가짐
-// const storedResults = testResults.concat([21, 1.1]); // push와는 다른 새로운 배열을 반환.
+// const storedResults = testResults.slice(0, 4); // * 배열을 복사, 새 주소를 가짐
+// const storedResults = testResults.concat([21, 1.1]); // * push와는 다른 새로운 배열을 반환.
 
 // storedResults.push(9, 11);
 
@@ -59,30 +59,30 @@
 // console.log("in", testResults.indexOf(3.3) !== -1);
 
 // const personData = [{ name: "A" }, { name: "B" }];
-// console.log(personData.indexOf({ name: "B" })); //객체가 있으면 실행되지 않음
+// console.log(personData.indexOf({ name: "B" })); // 객체가 있으면 실행되지 않음
 
 // const B = personData.find((person, idx, persons) => {
 //   return person.name === "B";
 // });
 
 // B.name = "DO";
-// // 동일한 객체의 동일한 참조 값이므로 find()는 복사를 생성하지 않음
+// * 동일한 객체의 동일한 참조 값이므로 find()는 복사를 생성하지 않음
 // console.log(B, personData);
 
 // const maxIndex = personData.findIndex((person, idx, persons) => {
 //   return person.name === "A";
-// }); // 해당 항목의 인덱스를 반환하는 것이 차이점
+// }); // * 해당 항목의 인덱스를 반환하는 것이 차이점
 // console.log(maxIndex);
 
 // const prices = [10.99, 5.99, 3.99, 9.99];
 // const tax = 0.99;
 // const taxAdjustedPrices = [];
 
-// // for (const price of prices) {
-// //   taxAdjustedPrices.push(price * (1 + tax));
-// // }
+// for (const price of prices) {
+//   taxAdjustedPrices.push(price * (1 + tax));
+// }
 
-// //for 반복문 대안
+// * for 반복문 대안
 // prices.forEach((price, idx, prices) => {
 //   const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
 //   taxAdjustedPrices.push(priceObj);
@@ -98,7 +98,7 @@
 //   return priceObj;
 // });
 
-// // console.log(prices, taxtAdjustedPrices);
+// console.log(prices, taxtAdjustedPrices);
 
 // const sortedPrices = prices.sort((a, b) => {
 //   if (a > b) {
@@ -109,22 +109,22 @@
 //     return 1;
 //   }
 // });
-// // console.log(sortedPrices.reverse());
+// console.log(sortedPrices.reverse());
 // console.log(sortedPrices);
 
-// //filter(), 가격이 6보다 큰
+//filter(), 가격이 6보다 큰
 // const filteredArray = prices.filter((price) => 6 < price);
 
 // console.log(filteredArray);
 
-// // let sum = 0;
-// // prices.forEach((price) => {
-// //   sum += price;
-// // });
+// let sum = 0;
+// prices.forEach((price) => {
+//   sum += price;
+// });
 
-// // console.log(sum);
+// console.log(sum);
 
-// //배열의 값을 단일값으로 할때 유용
+// * 배열의 값을 단일값으로 할때 유용
 // const sum = prices.reduce(
 //   (prevValue, curValue, curIndex, prices) => prevValue + curValue,
 //   0
--- setsmaps.js
+++ setsmaps.js
@@ -1,4 +1,4 @@
-// //set은 데이터 구조로 고유한 값을 관리할 때 유용
+// * Set은 데이터 구조로 고유한 값을 관리할 때 유용
 
 // const ids = new Set(["hi", "from", "set"]);
 // ids.add(2);
@@ -9,26 +9,35 @@
 // for (const entry of ids.entries()) {
 //   console.log(entry[0]);
 // }
-// //entries()대신 values()를 사용하면 Set 값을 한번만 출력하는 이터러블을 반환
+// * entries()대신 values()를 사용하면 Set 값을 한번만 출력하는 이터러블을 반환
 
-const person1 = { name: "Coco" };
-const person2 = { name: "Chacha" };
+// const person1 = { name: "Coco" };
+// const person2 = { name: "Chacha" };
 
-const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
+// const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
 
-personData.set(person2, [{ data: "two weeks ago", price: 100 }]);
+// personData.set(person2, [{ data: "two weeks ago", price: 100 }]);
 
-console.log(personData);
-console.log(personData.get(person1));
+// console.log(personData);
+// console.log(personData.get(person1));
 
-for (const [key, value] of personData.entries()) {
-  console.log(key, value);
-}
+// for (const [key, value] of personData.entries()) {
+//   console.log(key, value);
+// }
 
-for (const key of personData.keys()) {
-  console.log(key);
-}
+// for (const key of personData.keys()) {
+//   console.log(key);
+// }
 
-for (const value of personData.values()) {
-  console.log(value);
-}
+// for (const value of personData.values()) {
+//   console.log(value);
+// }
+
+/**
+ ** Maps (객체나 배열도 키로 가짐) 객체보다 유연성이 좋음
+ ** 정말 큰 데이터(데이터 양 많음)라면 객체보다 성능 좋음
+ ** 객체는 작거나, 중간크기의 데이터에 적합
+ ** 객체에 있는 데이터와 상호작용하는 메서드 Map에서 사용 불가
+ ** 초당 여러번 키-값 쌍을 추가, 삭제 하는 작업을 할때 좋음
+ ** Set, Map은 배열 객체보다 드물게 사용
+ */
